ba61d202ffeedc61f475ed1b3cb6c37460dcb934
Adriano Castro <49049819+AdrianoMktRJ@users.noreply.github.com>
2025-10-16 17:46:17 -0300

    Initialize TRPC client with HTTP batch link
    Add TRPC client setup with HTTP batch link and superjson transformer.

--- client/src/lib/trpc.ts
+++ client/src/lib/trpc.ts
@@ -1,4 +1,27 @@
 import { createTRPCReact } from "@trpc/react-query";
+import { httpBatchLink } from "@trpc/client";
 import type { AppRouter } from "../../../server/routers";
+import superjson from "superjson";
 
-export const trpc = createTRPCReact<AppRouter>();
+function getBaseUrl( ) {
+  if (typeof window !== "undefined") {
+    return window.location.origin;
+  }
+  // Vercel auto-populates this
+  if (process.env.VERCEL_URL) {
+    return `https://${process.env.VERCEL_URL}`;
+  }
+  // In a local dev environment
+  return `http://localhost:4000`;
+}
+
+export const trpc = createTRPCReact<AppRouter>( );
+
+export const trpcClient = trpc.createClient({
+  links: [
+    httpBatchLink({
+      url: `${getBaseUrl( )}/api/trpc`,
+      transformer: superjson,
+    }),
+  ],
+});
